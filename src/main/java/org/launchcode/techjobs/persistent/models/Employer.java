5ad9f509bb053bf9cae891cdebf333b2f72d9bc1
miccraig15 <miccraig15@gmail.com>
2023-02-22 11:51:13 -0600

    Created Abstract Entity Class, Filled Employer and Skill Classes, created Employer and Skill Repository

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max=30)
+    private String location;
+
+    public Employer() {}
+
+    public Employer (String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
