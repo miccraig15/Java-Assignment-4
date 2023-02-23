539ce01fdc3e55ddc138f74c9e41bed063dd32b5
miccraig15 <miccraig15@gmail.com>
2023-02-23 15:55:05 -0600

    Imported classes for AutoGrader

--- src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
+++ src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
@@ -7,6 +7,8 @@ import org.launchcode.techjobs.persistent.controllers.EmployerController;
 import org.launchcode.techjobs.persistent.controllers.SkillController;
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
